255baac06f5be3a66e5fee9f89bedb84c1fed063
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-09-14 14:12:38 +0200

    Revert emacs

--- host/home/default.nix
+++ host/home/default.nix
@@ -6,7 +6,7 @@
     ./config.nix # Networking, hardware options, nix options, user options
     ./hardware-configuration.nix # Pretty obvious right?
     ./packages.nix # Global packages
-    ./overlays.nix # emacs
+    #./overlays.nix # emacs
     ./services.nix # Xserver + Other services
   ];
 }
