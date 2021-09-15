7b9458194898151fd2e464c24fa8f9986c320866
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-09-15 21:07:09 +0200

    Update sha256 (Emacs 28)

--- host/laptop/overlays.nix
+++ host/laptop/overlays.nix
@@ -40,7 +40,9 @@
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
       #sha256= "0000000000000000000000000000000000000000000000000000";
-      sha256 = "16hj5dmw4wzgzb72f87p0cki8h1fv2fhxrxc2b7nncksjj833bv8";
+      #sha256 = "16hj5dmw4wzgzb72f87p0cki8h1fv2fhxrxc2b7nncksjj833bv8";
+      sha256 = "14vzfapq87xarpqfilbmi72dls77acv3hxa8m15p72h8q3xbgxsb";
+
     }))
 
 
