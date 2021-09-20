f0ab8991ac78df595adb767d5c1a542e995720c5
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-09-20 11:53:17 +0200

    Refactoring ¯\_(ツ)_/¯ pt. 3

--- common/default.nix
+++ common/default.nix
@@ -4,12 +4,14 @@
   imports = [
     # Handy way to make flake.nix less ripetitive
     ./bootloader.nix # bootloader options 
+    ./cachix.nix # cachix options 
     ./environment.nix # Environment variables + etc configuration
     ./fonts.nix # Fonts configuration
     ./locales.nix # Timezone and locales
     ./nix.nix # Nix package manager configuration & extras
     ./overrides.nix # dmenu patches
     ./packages.nix # Global packages
+    ./programs.nix # Global programs
     ./security.nix 
     ./shared.nix # Timezone, keyboard options, fonts, environment options, Flatpak
     ./user.nix # Username + extragroup
--- thinkpad.nix
+++ thinkpad.nix
@@ -3,8 +3,8 @@
 
   imports = [
     # Handy way to make flake.nix less ripetitive
-    ./options/default.nix
-    ./host/laptop/default.nix
-    ./common/default.nix
+    #./options/default.nix
+    ./host/laptop
+    ./common
   ];
 }
