dea72fe2dc72247dd5a094239b5f81475bbc9db2
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-09-21 18:35:14 +0200

    Edited sway configuration

--- common/programs.nix
+++ common/programs.nix
@@ -19,9 +19,10 @@
     sway =  {
       enable = true;
       wrapperFeatures.gtk = true;
+      #wrapperFeatures.base = true;
       extraPackages = with pkgs; [
         #swaylock # substituted with swaylock-effects
-        bemenu # dmenu for wayland
+        #bemenu # dmenu for wayland
         sxiv
         imv
         wl-clipboard
@@ -43,6 +44,19 @@
         swaylock-effects
 
       ];
+
+      #export SUDO_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      #export SSH_ASKPASS="${pkgs.ksshaskpass}/bin/ksshaskpass"
+      extraSessionCommands = ''
+      export MOZ_ENABLE_WAYLAND=1
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export XDG_SESSION_TYPE=wayland
+      export XDG_CURRENT_DESKTOP=sway
+    '';
+
     };
 
     #zsh = {
