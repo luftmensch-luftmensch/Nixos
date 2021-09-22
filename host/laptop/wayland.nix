2d366588728d07fb2735f2100c690c82dd291f88
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-09-22 18:50:27 +0200

    Change extraSessionCommand configuration

--- host/laptop/wayland.nix
+++ host/laptop/wayland.nix
@@ -36,7 +36,7 @@
     extraSessionCommands = ''
       export MOZ_ENABLE_WAYLAND=1
       export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
+      export QT_QPA_PLATFORM=qt5ct
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
       export _JAVA_AWT_WM_NONREPARENTING=1
       export XDG_SESSION_TYPE=wayland
