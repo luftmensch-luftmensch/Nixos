e4af37765d5f6cc65ee0da88f66e601bfe078377
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-09-23 10:16:54 +0200

    discord 0.0.15 -> 0.0.16

--- common/overlays.nix
+++ common/overlays.nix
@@ -5,7 +5,9 @@
   nixpkgs.overlays = [
     (import (builtins.fetchTarball {
       url = "https://github.com/InternetUnexplorer/discord-overlay/archive/main.tar.gz";
-      sha256= "0000000000000000000000000000000000000000000000000000";
+      #sha256= "0000000000000000000000000000000000000000000000000000";
+      sha256 = "1zr84m61dd685z55qrca59sbn2qwxai1lf927615dh312b5s7a51";
+
     }))
 
   ];
