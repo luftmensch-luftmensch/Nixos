3723f4c8ba180dcc4b76c3bbac33db0863bf6ddb
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-10-03 18:29:08 +0200

    Minor edits

--- host/laptop/services-laptop.nix
+++ host/laptop/services-laptop.nix
@@ -61,7 +61,7 @@
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_BAT="powersave";
-        CPU_SCALING_GOVERNOR_ON_AC="performance";
+        CPU_SCALING_GOVERNOR_ON_AC="powersave";
 
         CPU_MAX_PERF_ON_AC=75;
         CPU_MAX_PERF_ON_BAT=60;
