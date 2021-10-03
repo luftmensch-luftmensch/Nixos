f2bbb78dd9483aa5b1d21040c16fa095fd257f07
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-10-03 11:48:19 +0200

    Testing fonts

--- common/fonts.nix
+++ common/fonts.nix
@@ -1,20 +1,20 @@
 { lib, config, pkgs, ... }:
 {
   fonts.fonts = with pkgs; [
-    #dejavu_fonts
+    dejavu_fonts
     font-awesome
-    #fira-code
-    #liberation_ttf	
-    #google-fonts
-    #hack-font
-    (nerdfonts.override { fonts = [ "Iosevka" "FiraCode" ];  })
-    #noto-fonts-emoji 
+    fira-code
+    liberation_ttf	
+    google-fonts
+    hack-font
+    (nerdfonts.override { fonts = [ "Iosevka" ];  })
+    noto-fonts-emoji 
     source-code-pro
-    #vegur
+    vegur
     victor-mono
-    #cantarell_fonts
-    #inconsolata
-    #mononoki
-    #twemoji-color-font
+    cantarell_fonts
+    inconsolata
+    mononoki
+    twemoji-color-font
   ];
 }
