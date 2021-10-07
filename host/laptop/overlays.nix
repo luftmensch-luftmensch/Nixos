77105612e1316010312211843ba5c4e28e392d22
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-10-07 15:22:02 +0200

    Minor edits

--- host/laptop/overlays.nix
+++ host/laptop/overlays.nix
@@ -40,8 +40,7 @@
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
       #sha256= "0000000000000000000000000000000000000000000000000000";
-      #sha256 = "137a51jmk2h9mrp959ds6n1gdgg7wmvwhjxjjd6njcs40k38rshj";
-      sha256 = "04hb3isg95l34ibaf5gqcd4zy2imi4mmy9y84lq1jxg6310kvs4a";
+      sha256 = "1wsvw0xj5jw8dal2pyy59ahjhqaa72lkcvdr51wz5d2c5kzf0bk8";
 
     }))
 
