ee93469c5b8a0e16be8b77113851e3e220762959
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-11-05 11:21:31 +0100

    Set allowBrokenPkgs = true

--- common/overrides.nix
+++ common/overrides.nix
@@ -3,6 +3,7 @@
 
   nixpkgs.config = {
     allowUnfree = true;
+    allowBroken = true;
     packageOverrides = pkgs: rec {
       dmenu = pkgs.dmenu.override {
         patches = [ ./dmenu-patches/case-insensitive ./dmenu-patches/borders ./dmenu-patches/numbers];
