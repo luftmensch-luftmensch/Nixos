2979bb66d86be226d95679afb02030505b4f6f9e
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-11-07 10:31:22 +0100

    Testing

--- host/home/xorg.nix
+++ host/home/xorg.nix
@@ -5,7 +5,7 @@
   # Enable the X11 windowing system.
   services.xserver = {
     enable = true;
-    #videoDrivers = [ "nvidia"];
+    videoDrivers = [ "nvidia"];
     #videoDrivers = [ "nouveau"];
     displayManager= {
       sddm.enable = true;
