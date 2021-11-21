d070924d73a8fa9fb556384fd9f86ff640592b80
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-11-21 12:52:56 +0100

    Edit minori

--- common/overlays.nix
+++ common/overlays.nix
@@ -10,7 +10,8 @@
 
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      sha256 = "1fbz0j33f51sap44jc4w5p6r88gsiy89h5j412k31vb91a54fys3";
+      #sha256 = "1fbz0j33f51sap44jc4w5p6r88gsiy89h5j412k31vb91a54fys3";
+      sha256 = "18hm44cyqsc6lan54aaw9zf2s9b41myzlar3n8svs8w80maimcjb";
     }))
   ];
 }
