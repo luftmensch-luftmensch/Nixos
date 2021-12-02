e83bbee42885967d502660f962c0ed79781bd0e3
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-12-02 18:44:07 +0100

    flake.lock: Update
    Flake lock file changes:
    
    • Updated input 'emacs-overlay':
        'github:nix-community/emacs-overlay/8c7c2f86ce9efef1e02a2710f9f803b029c57b73' (2021-12-02)
      → 'github:nix-community/emacs-overlay/931eaf998fd6ebbbd662798b5756f962fb4cda6a' (2021-12-02)

--- common/overlays.nix
+++ common/overlays.nix
@@ -10,7 +10,7 @@
 
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      sha256 = "1bj4vryplcb85n5dwhil0ap9j1bk9f7zxd1rcz347zjxd3x997ws";
+      sha256 = "1izvpisn6s4q6kndch8f923b1q4nxzx71g4mc06swhh3di99zzsz";
     }))
   ];
 }
