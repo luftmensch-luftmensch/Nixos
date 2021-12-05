97598ca71a9dfebe0777ab08fa4a0cd84af99859
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-12-05 15:40:46 +0100

    Editing emacs overlay

--- common/overlays.nix
+++ common/overlays.nix
@@ -10,7 +10,8 @@
 
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      sha256 = "1izvpisn6s4q6kndch8f923b1q4nxzx71g4mc06swhh3di99zzsz";
+      #sha256 = "1izvpisn6s4q6kndch8f923b1q4nxzx71g4mc06swhh3di99zzsz";
+      sha256 = "0lx0nyip1dq8pzilbyw18364ain4k9qza3dlw0lhvfvb87iz4yp5";
     }))
   ];
 }
