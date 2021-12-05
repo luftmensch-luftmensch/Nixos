47aa4ca0a6881ccf6096d91ed42214a23a373003
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-12-05 20:59:58 +0100

    Minor edits

--- common/overlays.nix
+++ common/overlays.nix
@@ -10,8 +10,8 @@
 
     (import (builtins.fetchTarball {
       url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
-      #sha256 = "1izvpisn6s4q6kndch8f923b1q4nxzx71g4mc06swhh3di99zzsz";
-      sha256 = "0lx0nyip1dq8pzilbyw18364ain4k9qza3dlw0lhvfvb87iz4yp5";
+      #sha256 = "0lx0nyip1dq8pzilbyw18364ain4k9qza3dlw0lhvfvb87iz4yp5";
+      sha256 = "0awqhf4ry6f547zaay1im9dzvwi5b1zdp7grmq3kn6fivbp01m66";
     }))
   ];
 }
