e0c079c53410dfa1c026983d4b06e2a38d76b2d1
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-01-10 14:17:57 +0100

    Edited overlays

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,19 +1,13 @@
 { lib, config, pkgs, ... }:
 let
-  #emacsSHA = "01bwv052a8r1b66d477dd1nsc03rhwfckp5mq2yvnz2nh0f4jv8c";
-  #neovimSHA = "15b546l8zci56dgn19fiw8pa34b10ds58rlpw0y0iqxvlp4jhq4c";
-  emacsSHA = "0h8hhaykcczaqp4ygzysq0kry6rw1yh52d371wgn90wrwklfxm3g";
-  neovimSHA = "03vwck82arcbzdgf4afxynfa2nryn15n47hwh7lcf642x25m06gs";
+
+  emacsSHA = "17dz69i5sqpikzh51ccs99vdvkqibd4v55dg8hl01y708mwhcfpr";
+  neovimSHA = "1vhjdaq42vf3ppdrwbll65al29a56fvgf0cahjhk9jccbvikw9jg";
 
 in
 {
 
   nixpkgs.overlays = [
-    ### Used only as reference ###
-    #(import (builtins.fetchTarball {
-    #  url = "https://github.com/InternetUnexplorer/discord-overlay/archive/main.tar.gz";
-    #  sha256 = "169vamrbk3naviw20ig15206ks45sdldfriyf8imwfjl0s3myc7l";
-    #}))
 
     # Emacs Overlay
     (import (builtins.fetchTarball {
