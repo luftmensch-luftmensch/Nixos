9ce73d39f0232acc989c5a33be60f9a294f533b6
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-01-23 13:41:36 +0100

    Minor edits (overlays)

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,10 +1,9 @@
 { lib, config, pkgs, ... }:
 let
-  #emacsSHA = "11ig638la19sv6sxp8vdjgyi32p8d2b81sd9qrnpppl204wqlfcb";
-  #neovimSHA = "0skn4lh63srhr5i696zgdwvvv915pkyang7zqg9c3kvf7m15pnpk";
-
-  emacsSHA = "1kg58b4s9i3r6ir51j5kk1rszvrf69hl15y8sxf0rk07bz6mjjhv";
-  neovimSHA = "1bfhvwczpajh0w684brvz40ry2vsa9jrp8nlklpvrkg364w44829";
+  #emacsSHA = "1kg58b4s9i3r6ir51j5kk1rszvrf69hl15y8sxf0rk07bz6mjjhv";
+  #neovimSHA = "1bfhvwczpajh0w684brvz40ry2vsa9jrp8nlklpvrkg364w44829";
+  emacsSHA = "09dss9ajmpmn9vznfn547n9fa14bqyz59912kmcw39lzifj5k0fl";
+  neovimSHA = "04q1djy87h44yfm4866d2vvqkgzvjks31mhgc1wvr7csly6ghzh5";
 in
 {
 
