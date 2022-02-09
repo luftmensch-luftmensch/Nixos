7c97ce7f14fe87cd70c8d5cd408c3353bfd0a510
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-02-09 13:29:05 +0100

    Minor edits

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,10 +1,10 @@
 { lib, config, pkgs, ... }:
 let
-  #emacsSHA = "0m5n1ylsx5pjn6xzp13kmkykds6kd7plm9svj78izsnz2240yas2";
-  #neovimSHA = "08v627zlqna0ccg8064qs76qg3914w1srw5jjxx1lqkn6mgdzskg";
+  #emacsSHA = "076fadagzhsfjlgj2qk6xr9czhpljfacw7r8g4mmxzpg91s9qngr";
+  #neovimSHA = "1cwg4ix5xns4g38hw4zxgk725p0v1ypqa1ss1pkgmhrqdw93dbc1";
 
-  emacsSHA = "076fadagzhsfjlgj2qk6xr9czhpljfacw7r8g4mmxzpg91s9qngr";
-  neovimSHA = "1cwg4ix5xns4g38hw4zxgk725p0v1ypqa1ss1pkgmhrqdw93dbc1";
+  emacsSHA = "05mhf6i0d7yiq8zixlhg0wb73xn4g07di29ik0rxgyh4q9sv5mxr";
+  neovimSHA = "0fzyg1z7j4xhvxxkqb59vgx94ryngsjk1j2jvd4fclj9bgyxy4zs";
 in
 {
 
--- common/services.nix
+++ common/services.nix
@@ -87,6 +87,7 @@
 
       # Set noop scheduler for zfs partitions
       #KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
+      #ACTION=="add|change", KERNEL=="sd[a-z]*[0-9]*|mmcblk[0-9]*p[0-9]*|nvme[0-9]*n[0-9]*p[0-9]*", ENV{ID_FS_TYPE}=="zfs_member", ATTR{../queue/scheduler}="none"
     };
 
     #udiskie = {
