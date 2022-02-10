9c19882c54778bc9226a21462601f7381c534f2d
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-02-10 12:25:56 +0100

    Set cached overlays

--- common/overlays.nix
+++ common/overlays.nix
@@ -1,20 +1,25 @@
 { lib, config, pkgs, ... }:
 let
-  #emacsSHA = "05mhf6i0d7yiq8zixlhg0wb73xn4g07di29ik0rxgyh4q9sv5mxr";
-  #neovimSHA = "0fzyg1z7j4xhvxxkqb59vgx94ryngsjk1j2jvd4fclj9bgyxy4zs";
-
-  emacsSHA = "0bqw75xf5slvf5w6z16z4b0pdg5j7azf26jijjnzb0gvznxdkn02";
-  neovimSHA = "1s6ggrrwry31vpkkiwnjn4yaih555srpccjvykshjdy0n4cp9vjy";
+  emacsSHA = "1bwsh31n4qgzj2q47h0nb7wcpnk6ns07n3aivy5s22kfncvarydz";
+  neovimSHA = "1qz6fy9j50jgqv9phsrfn3lx9vs05qrny6q1fz2i5fa04wyxv5m2";
 in
 {
 
   nixpkgs.overlays = [
+    # In order to get the latest successful build cached go to https://hydra.nix-community.org/project/emacs-overlay (stable branch)
+    # Get the latest succeful build for emacsPgtkGcc  -> inputs -> get revision and change it into url version
 
-    # Emacs Overlay
-    (import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+    #Emacs overlay
+    (import (builtins.fetchTarball{
+      url = "https://github.com/nix-community/emacs-overlay/archive/26aa8eb73f43ae039c2407ff5bc798cad67bf39d.tar.gz";
       sha256 = "${emacsSHA}";
     }))
+
+    # Emacs Overlay
+    #(import (builtins.fetchTarball {
+    #  url = "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+    #  sha256 = "${emacsSHA}";
+    #}))
 
     ## Neovim overlay
     (import (builtins.fetchTarball {
