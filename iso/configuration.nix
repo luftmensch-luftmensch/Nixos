19025629cf7edd9ae6b53d2f63abed6ece56409a
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-04-21 19:12:48 +0200

    Minor edits

--- iso/configuration.nix
+++ iso/configuration.nix
@@ -1,4 +1,13 @@
 { pkgs, ... }: {
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Enable SSH in the boot process.
+  systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+
+  #users.users.root.openssh.authorizedKeys.keys = [
+  #"ssh-ed25519 AaAeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee username@host" # Take from the id_rsa.pub file on ~/.ssh
+  #];
+
   nix = {
     package = pkgs.nixUnstable;
     extraOptions = ''
