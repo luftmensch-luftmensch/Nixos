18d0e6cbe63ec49c051e5e80d54b45c35c635864
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2022-06-14 17:46:57 +0200

    Revert

--- common/shared.nix
+++ common/shared.nix
@@ -5,10 +5,10 @@
     portal = {
       enable = true;
       wlr.enable = true;
-      extraPortals = with pkgs; [
+      #extraPortals = with pkgs; [
       #  xdg-desktop-portal-wlr
-        xdg-desktop-portal-gtk
-      ];
+      #  xdg-desktop-portal-gtk
+      #];
       #gtkUsePortal = true;
     };
   };
